71ac4ba820eeb3ba1d9644ba420f5016f7f73719
SUMESH M <sumeshm007@gmail.com>
2025-02-15 19:53:19 +0530

    hello

--- application.py
+++ application.py
@@ -38,4 +38,4 @@ def predict_datapoint():
         return render_template("home.html",prediction=results[0])
     
 if __name__=="__main__":
-    app.run(host='0.0.0.0',debug=False)
+    app.run(host='0.0.0.0')
